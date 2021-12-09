33a3240f29824845b65a7b0bc214ff0dc91b7e3a
Haythem Ben Drissia <hbendrissia@vermeg.com>
2021-12-09 12:36:49 +0100

    data binding in Angular Forms

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-settings-form.component.css']
 })
 export class UserSettingsFormComponent implements OnInit {
+
+  originalUserSettings: UserSettings = {
+    name: "Haythem",
+    emailOffers: true,
+    interfaceStyle: "dark",
+    subscriptionType: "Annual",
+    notes: "This is a note"
+  };
+
+  userSettings: UserSettings = { ...this.originalUserSettings };
 
   constructor() { }
 
